6110cd57feb8eac2c8b45248960627e5608b3b06
Gabriell Rossolon <gabrirossolon@yahoo.com.br>
2024-02-01 14:30:29 -0400

    mod 03 ok

---
+++ app/reconhecientoDeVoz.js
@@ -0,0 +1,31 @@
+const elementoChute = document.getElementById('chute');
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-Br'
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(numeroFalado) {
+    chute = numeroFalado.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela() {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+  
+
+
+
+
+
+
